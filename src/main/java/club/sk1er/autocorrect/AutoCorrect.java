fc4c33cdc4a54a84203ee59ae4a49c3606af2a4a
asbyth <cyronize@gmail.com>
2020-02-23 23:24:18 -0500

    add modcore

--- src/main/java/club/sk1er/autocorrect/AutoCorrect.java
+++ src/main/java/club/sk1er/autocorrect/AutoCorrect.java
@@ -1,14 +1,22 @@
 package club.sk1er.autocorrect;
 
+import club.sk1er.modcore.ModCoreInstaller;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import org.languagetool.JLanguageTool;
 import org.languagetool.MultiThreadedJLanguageTool;
 import org.languagetool.language.AmericanEnglish;
 
-@Mod(modid = "autocorrect", name = "AutoCorrect", version = "1.1")
+@Mod(modid = "autocorrect", name = "AutoCorrect", version = "1.2")
 public class AutoCorrect {
     @Mod.Instance
     public static AutoCorrect INSTANCE;
 
     public JLanguageTool checker = new MultiThreadedJLanguageTool(new AmericanEnglish());
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        ModCoreInstaller.initializeModCore(Minecraft.getMinecraft().mcDataDir);
+    }
 }
--- src/main/java/club/sk1er/autocorrect/tweaker/AutoCorrectTweaker.java
+++ src/main/java/club/sk1er/autocorrect/tweaker/AutoCorrectTweaker.java
@@ -1,14 +1,30 @@
 package club.sk1er.autocorrect.tweaker;
 
+import club.sk1er.modcore.ModCoreInstaller;
+import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 import java.util.Map;
 
 @IFMLLoadingPlugin.SortingIndex(1000)
-public class AutoCorrectTweaker implements IFMLLoadingPlugin{
+@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
+public class AutoCorrectTweaker implements IFMLLoadingPlugin {
     @Override
     public String[] getASMTransformerClass() {
-        return new String[]{ ClassTransformer.class.getName() };
+        int initialize = ModCoreInstaller.initialize(Launch.minecraftHome, "1.8.9");
+
+        if (ModCoreInstaller.isErrored() || initialize != 0 && initialize != -1) {
+            // Technically wouldn't happen in simulated installed but is important for actual impl
+            System.out.println("Failed to load Sk1er Modcore - " + initialize + " - " + ModCoreInstaller.getError());
+        }
+
+        // If true the classes are loaded
+        if (ModCoreInstaller.isIsRunningModCore()) {
+            return new String[]{"club.sk1er.mods.core.forge.ClassTransformer", ClassTransformer.class.getName()};
+        }
+
+        return new String[]{ClassTransformer.class.getName()};
     }
 
     @Override
